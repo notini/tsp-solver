ce6889411c4cd2fbefc1c3aa42e46f5183fe8415
Vitor Notini Pontes <vitornotini@gmail.com>
2020-06-24 16:08:08 -0300

    Updates

---
+++ precache-manifest.b22fef62a0b804009bbd192794733981.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b58b45edbc6e658a7ef0",
+    "url": "/tsp-solver/static/css/main.5fee4992.chunk.css"
+  },
+  {
+    "revision": "b58b45edbc6e658a7ef0",
+    "url": "/tsp-solver/static/js/main.b58b45ed.chunk.js"
+  },
+  {
+    "revision": "6b345c639d952febc94f",
+    "url": "/tsp-solver/static/css/1.03df134d.chunk.css"
+  },
+  {
+    "revision": "6b345c639d952febc94f",
+    "url": "/tsp-solver/static/js/1.6b345c63.chunk.js"
+  },
+  {
+    "revision": "dc7d4bd4a780c5c8ab8c",
+    "url": "/tsp-solver/static/js/runtime~main.dc7d4bd4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tsp-solver/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1afcbd6a1c454e7b7f0bc58155558b9d",
+    "url": "/tsp-solver/static/media/academicons.1afcbd6a.ttf"
+  },
+  {
+    "revision": "ea3755c311d0678ca4f041912a9039d6",
+    "url": "/tsp-solver/static/media/academicons.ea3755c3.eot"
+  },
+  {
+    "revision": "406b38d29cffe8ffcb7962faa2e095ef",
+    "url": "/tsp-solver/static/media/academicons.406b38d2.svg"
+  },
+  {
+    "revision": "2552b354cabf01815628a244fde0636e",
+    "url": "/tsp-solver/static/media/academicons.2552b354.woff"
+  },
+  {
+    "revision": "90567a290feac3a7746430ad9f303ea3",
+    "url": "/tsp-solver/index.html"
+  }
+];
